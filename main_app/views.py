bdc6fae8265e44d64f2372e89d92bec32937489b
fmher <feumatthewher2@gmail.com>
2023-02-13 11:04:53 -0800

    can now crud

---
+++ main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+from .models import Cars
+
+# Define the home view
+def home(request):
+    return HttpResponse('<h1> (╯°□°）╯︵ ┻━┻ </h1>')
+
+def cars(request):
+    cars = Cars.objects.all()
+    # messed up on DB name, so it is carss
+    return render(request, 'cars_list.html', {'carss':cars})
