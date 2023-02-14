315c882cb478788367f19af5dd24a3d1361bb77a
fmher <feumatthewher2@gmail.com>
2023-02-13 16:22:51 -0800

    decided to give each car a page

--- main_app/views.py
+++ main_app/views.py
@@ -8,10 +8,18 @@ from .models import Cars
 def home(request):
     # return HttpResponse('<h1> (╯°□°）╯︵ ┻━┻ </h1>')
     cars = Cars.objects.all()
+    # named db cars so in django db its carss
     return render(request, 'welcome_page.html', {'carss':cars})
 
 def cars_info(request, pk):
+    # find whichevers been clicked 
     info = Cars.objects.filter(pk = pk)
     all_cars = Cars.objects.all()
-    # info is only one car and its info
-    return render(request, 'car_info.html', {'Cars':info, 'carss':all_cars})
+    # decided to give car its own page, due to possible diff pic size and quality
+    if (pk <= 1):
+        return render(request, 'model_2.html', {'Cars':info, 'carss':all_cars})
+    elif (pk <= 2):
+        return render(request, 'model_s.html', {'Cars':info, 'carss':all_cars} )
+    elif (pk <= 3):
+        return render(request, 'model_l.html', {'Cars':info, 'carss':all_cars} )
+    return render(request, 'model_o.html', {'Cars':info, 'carss':all_cars})
