ad8f748c2ac4259c2a52b036e4fdd848ce1a06cd
fmher <feumatthewher2@gmail.com>
2023-04-12 15:14:06 -0700

    got ready for deploy on vercel

--- project4/settings.py
+++ project4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4*io9*54mqcoi*9m%@&u4dtj#y7o_p6k$gmh8!^kfa30t^ajui
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
